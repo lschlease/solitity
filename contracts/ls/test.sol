eb162641e031243ebb35c86b411de877abd690cc
lschlease <343196844@qq.com>
2024-05-26 23:35:40 +0800

    test

---
+++ contracts/ls/test.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.24 and less than 0.9.0
+pragma solidity ^0.8.24;
+
+//  0x1c91347f2A44538ce62453BEBd9Aa907C662b4bD.   合约
+// 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4。 账户地址
+
+contract founMe {
+    address public onwer;
+
+
+    function fund() public payable {}
+
+    function give() public  {}
+}
